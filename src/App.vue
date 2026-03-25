272c3f60d5f1db2199c9204196997063391f3a71
septaalfauzan <engineering@Admins-MacBook-Air.local>
2026-03-25 15:26:49 +0700

    feat: add download my resume button

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,23 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-
     <div class="wrapper">
       <h1>Hello, Im Septa Alfauzan</h1>
       <h2>Mobile Developer specializing in native and multiplatform app development</h2>
-      <p>I build production-ready apps used by real users — from SIM management platforms to IoT systems and enterprise tools.
-
-Currently working with international clients and shipping apps to Play Store & App Store.</p>
-
+      <p>With 2+ years experience in mobile development, I build production-ready apps used by real users — from SIM management platforms to IoT systems and enterprise tools. Currently working with international clients and shipping apps to Play Store & App Store.</p>
+      <a href="https://drive.google.com/file/d/1Ubv2crCzDHV0yKea2qPwq3jtaPdim1vm/view?usp=sharing" 
+      target="blank"
+      class="btn btn-default btn-ghost" >Download My Resume</a>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">/</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/experiences">Experiences</RouterLink>
         <RouterLink to="/skills">Skills</RouterLink>
         <RouterLink to="/contacts">Contacts</RouterLink>
       </nav>
     </div>
-  </header>
-  <div style="margin-top: 32px;">
+
+  <div id="router-content">
     <RouterView/>
   </div>
 </template>
